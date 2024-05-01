eb475fdd22f45002ed0fe8498b244c695d791fb1
Md Mahdin Moin <mdmahdinmoin738a@gmail.com>
2024-05-01 14:12:39 +0600

    done

--- numberconversion.js
+++ numberconversion.js
@@ -1,8 +1,14 @@
-let age = 17;
-let name = "Mahdin";
-let doTheWork = true;
+// let age = 17;
+// let name = "Mahdin";
+// let doTheWork = true;
 
-ageInString = String(age)
-console.log(ageInString)
+// ageInString = String(age)
+// console.log(ageInString)
+// nameInBoolean = Boolean(name)
 
-nameInBoolean = Boolean(name);
+let str1 = "1"
+let str2 = 2
+console.log(str1 + str2)
+
+console.log(+"")
+
