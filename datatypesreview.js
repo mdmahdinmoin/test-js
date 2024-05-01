78ebed1cdbee0eb36a71e9ea74764eacdffe1de6
Md Mahdin Moin <mdmahdinmoin738a@gmail.com>
2024-05-01 19:36:33 +0600

    reviews

---
+++ datatypesreview.js
@@ -0,0 +1,29 @@
+// primitive => string ,number,boolean,null,undefined,symbol, BigInt
+
+let name = "Mahdin"
+let age = 17
+let userloggedIn = false;
+let temp = null;
+let state;
+
+const hablu = Symbol("123")
+const babul = Symbol("123")
+
+let bigNumber = 345678901239057389998202891n
+
+
+// reference value => arrays, objects, funtions
+
+let heroes =["CapAmerica", "IronMan", "Thor", "Hulk"];
+let myObj ={
+    nameCa : "store",
+    value: "amazing",
+}
+
+// const myFunction = funtion(){
+//     console.log(134);
+// }
+
+console.log(typeof(state));
+console.log(hablu === babul)
+
