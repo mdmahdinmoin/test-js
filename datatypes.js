ae62d4eade0b19e96c3e25f340b8185384cb30c6
Md Mahdin Moin <mdmahdinmoin738a@gmail.com>
2024-05-01 11:34:03 +0600

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+let name = "mahdin";
+let age = 17;//2to power 53,bigint
+let userLoggedIn = true;
+let state;
+//null standalone value
+console.log(typeof state);
+console.log(typeof city);
---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 14599
+let accountEamil = "mah@google.com"
+var accountPassword = 1246
+
+let accountState;
+
+console.table([accountId, accountEamil, accountPassword, accountCity, accountState])
+
