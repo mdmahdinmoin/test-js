88551d764b200adeb5e5f34605032539c869e619
Md Mahdin Moin <mdmahdinmoin738a@gmail.com>
2024-05-03 10:58:20 +0600

    string done

---
+++ string.js
@@ -0,0 +1,24 @@
+// const name = "Mahdin "
+// const repo = 50
+// console.log(name + repo + " Value")
+// console.log(`My name is ${name} and my repo is ${repo}`)
+
+const user = new String("Mahdin-md-com")
+// console.log(user.__proto__)
+// console.log(user);
+
+// console.log(user.charAt(3));
+// console.log(user.indexOf("h"))
+
+// console.log(user.substring(0,5));
+// console.log(user.slice(-8,4));
+
+// const id = "  Mahdin  "
+// console.log(id.trim());
+
+const url = "https:/MdMoin/kool%20Wow";
+console.log(url.replace("%20", "-"));
+console.log(url.split("%"));
+
+
+
