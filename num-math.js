b2d97f2057892cc29d6ac1e18c356c5005002f0e
Md Mahdin Moin <mdmahdinmoin738a@gmail.com>
2024-05-03 14:19:23 +0600

    math  done

---
+++ num-math.js
@@ -0,0 +1,25 @@
+// const score = 100
+// console.log(score);
+// const newScore = new Number(score)
+// console.log(newScore);
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+// const digit = 10000000000000
+// console.log(digit.toLocaleString("en-IN"))
+
+
+// console.log(Math.abs(4));
+// console.log(Math.round(4.8));
+// console.log(Math.floor(4.8))
+// console.log(Math.min(12,16,19,69));
+
+console.log(Math.random());
+console.log((Math.random()*10 )+1);
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
